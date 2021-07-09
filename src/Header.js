54be0783dc8edbf5876a1640437387652607c0b0
roneetsingh <kaket786@gmail.com>
2021-07-09 21:25:21 +0530

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header className="text-center">
+            <img src="https://purepng.com/public/uploads/medium/big-chungus-z7y.png"
+            alt="Problem?" class="center"
+            />
+            <h1>Roneet's Meme Bucket</h1>
+        </header>
+    )
+}
+
+export default Header
